1a2e193618d658b9e013ffab32255019c81489c5
perlancar (@pc-home) <perlancar@gmail.com>
2015-03-22 20:27:36 +0700

    [doc] Add description, mention Ledger::Parser

--- lib/Ledger/Examples.pm
+++ lib/Ledger/Examples.pm
@@ -6,6 +6,14 @@ package Ledger::Examples;
 1;
 # ABSTRACT: Example Ledger files
 
+=head1 DESCRIPTION
+
+This distribution contains various examples of valid and invalid Ledger journal
+files in its per-dist share dir.
+
+
 =head1 SEE ALSO
+
+L<Ledger::Parser>
 
 L<http://www.ledger-cli.org>
